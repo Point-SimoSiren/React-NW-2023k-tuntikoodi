2752e49a54161d74c734fca97e49e73a5912fb18
Simo Sirén <simo.siren1@careeria.fi>
2023-04-19 09:42:57 +0300

    user add aloitettu

---
+++ src/UserAdd.jsx
@@ -0,0 +1,113 @@
+import "./App.css"
+import React, {useState} from 'react'
+import UserService from './Services/User'
+
+// Otetaan vastaan propsina setLisäystila ( = staten muutosfunktio) kutsuvalta List komponentilta
+const UserAdd = ({setLisäystila, x, reload, setShowMessage, setMessage, setIsPositive}) => {
+
+// Komponentin tilassa on input kenttien sen hetkinen sisältö
+const [newFirstName, setNewFirstName] = useState('')
+const [newLastName, setNewLastName] = useState('')
+const [newUserName, setNewUserName] = useState('')
+const [newPassword, setNewPassword] = useState('')
+const [newPhone, setNewPhone] = useState('')
+const [newAccessLevelId, setNewAccessLevelid] = useState(2)
+
+
+// onSubmit tapahtumankäsittelijä funktio
+const handleSubmit = (event) => {
+    event.preventDefault()
+    var newUser = {
+      firstName: newFirstName,
+      lastName: newLastName,
+      userName: newUserName,
+      password: newPassword,
+      phone: newPhone,
+      accessLevelId: newAccessLevelId
+  }
+  
+  CustomerService.create(newCustomer)
+  .then(response => {
+    if (response.status === 200) {
+           
+            setMessage(response.data)
+            setIsPositive(true)
+            setShowMessage(true)
+
+            setTimeout(() => {
+                setShowMessage(false)
+            }, 4000);
+
+            reload(!x)
+            setLisäystila(false)
+    }
+})
+.catch(error => {
+    setMessage(error.message)
+    setIsPositive(false)
+    setShowMessage(true)
+
+    setTimeout(() => {
+      setShowMessage(false)
+     }, 6000)
+  })
+}
+
+
+return(
+    <div id="addNew">
+
+    <h2>Adding new Customer</h2>
+
+    <form onSubmit={handleSubmit}>
+    <div>
+             <input type="text" value={newCustomerId} placeholder="ID with 5 capital letters" maxLength="5" minLength="5"
+                 onChange={({ target }) => setNewCustomerId(target.value)} required />
+         </div>
+         <div>
+             <input type="text" value={newCompanyName} placeholder="Company name"
+                 onChange={({ target }) => setNewCompanyName(target.value)} required />
+         </div>
+         <div>
+             <input type="text" value={newContactName} placeholder="Contact name"
+                 onChange={({ target }) => setNewContactName(target.value)} />
+         </div>
+         <div>
+             <input type="text" value={newContactTitle} placeholder="Contact title"
+                 onChange={({ target }) => setNewContactTitle(target.value)} />
+         </div>
+         <div>
+             <input type="text" value={newCountry} placeholder="Country"
+                 onChange={({ target }) => setNewCountry(target.value)} />
+         </div>
+         <div>
+             <input type="text" value={newAddress} placeholder="Address"
+                 onChange={({ target }) => setNewAddress(target.value)} />
+         </div>
+         <div>
+             <input type="text" value={newCity} placeholder="City"
+                 onChange={({ target }) => setNewCity(target.value)} />
+         </div>
+         <div>
+             <input type="text" value={newPostalCode} placeholder="Postal code"
+                 onChange={({ target }) => setNewPostalCode(target.value)} />
+         </div>
+         <div>
+             <input type="text" value={newPhone} placeholder="Phone"
+                 onChange={({ target }) => setNewPhone(target.value)} />
+         </div>
+         <div>
+             <input type="text" value={newFax} placeholder="Fax"
+                 onChange={({ target }) => setNewFax(target.value)} />
+         </div>
+      
+      <input type='submit' value='save' />
+      <input type='button' value='back' onClick={() => setLisäystila(false)} />
+    </form>
+
+ </div>
+)
+
+}
+
+export default CustomerAdd
